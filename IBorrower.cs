a1a0b375552b4c196feb7b5fb74958414e1a9d53
thilomoi05 <thiloth@uia.no>
2026-03-08 21:49:24 +0100

    Adds IBorrower

---
+++ IBorrower.cs
@@ -0,0 +1,8 @@
+namespace UniversitetsSystem
+{
+    public interface IBorrower // Interface som sier hvilke metoder som må brukes
+    {
+        string GetBorrowerId();
+        string GetBorrowerName();
+    }
+}
